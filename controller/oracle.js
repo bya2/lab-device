66eebb6b6be04b09a96f74621172c6837eb7960e
bya2 <byaa1972@gmail.com>
2021-11-23 00:31:13 +0900

    Write the query to change according to the change in the table columns

--- controller/oracle.js
+++ controller/oracle.js
@@ -1,21 +1,20 @@
 "use strict";
 
-const process = require('process');
-const oracledb = require('oracledb');
+const process = require("process");
+const oracledb = require("oracledb");
 
-const oracleConfig = require('../config/oracle');
+const oracleConfig = require("../config/oracle");
+
+const CONST_NUM_OF_SENSORS = 6;
 
 const oracleCtrl = {
-  fnCreatePool: async function(_config)
-  {
+  fnCreatePool: async function (_config) {
     this.pool = await oracledb.createPool(_config || oracleConfig);
   },
-  fnGetConnection: async function()
-  {
+  fnGetConnection: async function () {
     this.conn = await oracledb.getConnection();
   },
-  fnOperInAdvance: async function(_config)
-  {
+  fnOperInAdvance: async function (_config) {
     if (_config || oracleConfig) {
       await this.fnCreatePool(_config);
       await this.fnGetConnection();
@@ -24,29 +23,35 @@ const oracleCtrl = {
       process.exit(1);
     }
   },
-  fnDMLInsert: async function(_sec, _ssn, _obj)
-  {
-    const q = `INSERT INTO ${_sec}${_ssn} VALUES (:MIN, :MAX, :AVG, :LEN, SYSDATE)`;
-    const bindParams = [_obj.min, _obj.max, _obj.avg, _obj.len];
+  fnDMLInsert: async function (_sec, _arrObj) {
+    // 섹터(_sec)의 수에 따라 유동적으로 INSERT를 할 수 있도록 쿼리 작성.
+    // 테이블 컬럼의 변경에도 무난하게 작동.
+
+    // 현재 AVG(평균)값만 데이터베이스 테이블에 INSERT.
+
+    let q; // INSERT INTO ${_sec} VALUES (:AVG1,:AVG2,:AVG3,:AVG4,:AVG5,:AVG6)
+    const bindParams = [];
     const qOpts = {
       autoCommit: true,
-      bindDefs: {
-        MIN: { type: this.oracledb.DB_TYPE_NUMBER },
-        MAX: { type: this.oracledb.DB_TYPE_NUMBER },
-        AVG: { type: this.oracledb.DB_TYPE_NUMBER },
-        LEN: { type: this.oracledb.DB_TYPE_NUMBER },
-      },
+      bindDefs: {},
     };
+
+    q = `INSERT INTO ${_sec} VALUES (`;
+    for (let i = 0; i < CONST_NUM_OF_SENSORS; ++i) {
+      q += `:AVG${i},`;
+      bindParams = [...bindParams, _arrObj[i].avg];
+      qOpts.bindDefs[`AVG${i}`] = { type: oracledb.DB_TYPE_NUMBER };
+    }
+    q += "SYSDATE)";
+
     await this.conn.execute(q, bindParams, qOpts);
   },
-  fnCloseConnection: async function()
-  {
+  fnCloseConnection: async function () {
     if (this.conn) {
       await this.conn.close();
     }
   },
-  fnOperAtTerminatation: async function()
-  {
+  fnOperAtTerminatation: async function () {
     try {
       await this.fnCloseConnection();
       await oracledb.getPool().close(10);
@@ -55,6 +60,6 @@ const oracleCtrl = {
       process.exit(1);
     }
   },
-}
+};
 
 module.exports = oracleCtrl;
