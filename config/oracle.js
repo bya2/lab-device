bb87f9f7e1c1b05a5531688236bb622699aaa306
bya2 <byaa1972@gmail.com>
2022-01-18 12:58:06 +0900

    update

--- config/oracle.js
+++ config/oracle.js
@@ -1,5 +1,5 @@
 module.exports = {
   user: process.env.NODE_ORACLEDB_USER,
   password: process.env.NODE_ORACLEDB_PASSWORD,
-  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING_1522, // 오라클 포트 1522 (1521일 경우, process.env.NODE_ORACLEDB_CONNECTIONSTRING_1521)
+  connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING,
 };
